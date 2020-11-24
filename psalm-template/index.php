88332d6f10dbb721454667483e6495088e688da9
Nyholm <tobias.nyholm@gmail.com>
2020-11-24 10:56:20 +0100

    Comment

--- psalm-template/index.php
+++ psalm-template/index.php
@@ -4,5 +4,8 @@ require dirname(__DIR__).'/vendor/autoload.php';
 
 
 $repository = new \PsalmTemplate\MyEntityRepository();
+
+// PhpStorm 2020.3 Beta (Build #PS-203.5981.43, built on November 19, 2020) has
+// no idea what $entity is.
 $entity = $repository->getOne();
 $entity->bar();
